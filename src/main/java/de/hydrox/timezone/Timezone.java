366ca68ffb03c45b714eb54c17fdf43ba0f7da28
Matthias Söhnholz <miniminko@googlemail.com>
2011-04-07 02:15:12 +0200

    first implementation of the timezone conversion

---
+++ src/main/java/de/hydrox/timezone/Timezone.java
@@ -0,0 +1,50 @@
+package de.hydrox.timezone;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Timezone extends JavaPlugin {
+
+	private static final Logger log = Logger.getLogger("Minecraft");
+
+	public void onEnable() {
+		log.info("[Timezone] Timezone loaded");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		if (args.length >= 3) {
+			DateFormat df = DateFormat.getDateInstance();
+			try {
+				df.setTimeZone(TimeZone.getTimeZone(args[0]));
+				df.parse(args[2] + ", " + args[0]);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			DateFormat serverFormat = new SimpleDateFormat();
+			DateFormat firstFormat = new SimpleDateFormat();
+
+			TimeZone zone = TimeZone.getTimeZone(args[0]);
+			sender.sendMessage("Servertime:" + serverFormat.format(df));
+			
+			for (String string : args) {
+				zone = TimeZone.getTimeZone(string);
+				firstFormat.setTimeZone(zone);
+				sender.sendMessage("-->"+string+": " + firstFormat.format(df));
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public void onDisable() {
+		log.info("[Timezone] Timezone unloaded");
+	}
+
+}
